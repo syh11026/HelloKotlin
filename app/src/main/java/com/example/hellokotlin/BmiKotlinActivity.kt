9ff989533718477ee5112b1b208249266fbfb19b
syh11026 <thsdbgus11026@naver.com>
2021-04-28 14:44:01 +0900

    Kotlin practice

--- app/src/main/java/com/example/hellokotlin/BmiKotlinActivity.kt
+++ app/src/main/java/com/example/hellokotlin/BmiKotlinActivity.kt
@@ -17,12 +17,16 @@ class BmiKotlinActivity : AppCompatActivity() {
         val txtResult = findViewById<TextView>(R.id.txtResult)
 
         btnBmi.setOnClickListener {
+            try {
             val height = heightField.getText().toString().toDouble()
             val weight = weightField.getText().toString().toDouble()
 
             val bmi = weight / Math.pow(height / 100.0, 2.0)
 
             txtResult.setText("Your BMI =" + bmi)
+        } catch(e : Exception) {
+                txtResult.setText("Input(s) missing" )
+            }
         }
     }
 }
